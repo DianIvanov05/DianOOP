5ea58f7551a43c5dfbe8ae521f231f14f0c34e6b
Preslava-P <preslavap20@gmail.com>
2023-02-15 11:22:47 +0200

    first commit

---
+++ Book/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book
+{
+    public class Book:IComparable<Book>
+    {
+        public int CompareTo(Book other)
+        {
+            int result = this.Year.CompareTo(other.Year);
+            if (result == 0)
+            {
+                result = this.Title.CompareTo(other.TItle);
+            }
+            return result;
+        }
+    }
+}
